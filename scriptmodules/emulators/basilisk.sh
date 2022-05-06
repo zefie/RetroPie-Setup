c5cbb2a06ba94bfe790783b5995cdb338418fd35
zefie <zefie@zefie.net>
2022-05-06 02:18:19 -0400

    update basiliskII sources and script

--- scriptmodules/emulators/basilisk.sh
+++ scriptmodules/emulators/basilisk.sh
@@ -11,14 +11,14 @@
 
 rp_module_id="basilisk"
 rp_module_desc="Macintosh emulator"
-rp_module_help="ROM Extensions: .img .rom\n\nCopy your Macintosh roms mac.rom and disk.img to $romdir/macintosh"
-rp_module_licence="GPL2 https://raw.githubusercontent.com/cebix/macemu/master/BasiliskII/COPYING"
-rp_module_repo="git https://github.com/cebix/macemu.git master"
+rp_module_help="ROM Extensions: .img .rom\n\nCopy your Macintosh roms to $biosdir/classicmac.rom and classic_disk.img to $romdir/macintosh"
+rp_module_licence="GPL2 https://raw.githubusercontent.com/kanjitalk755/macemu/master/BasiliskII/COPYING"
+rp_module_repo="git https://github.com/kanjitalk755/macemu.git master"
 rp_module_section="opt"
-rp_module_flags="sdl1 !mali"
+rp_module_flags="sdl2 !mali"
 
 function depends_basilisk() {
-    local depends=(libsdl1.2-dev autoconf automake oss-compat)
+    local depends=(libsdl2-dev autoconf automake oss-compat)
     isPlatform "x11" && depends+=(libgtk2.0-dev)
     getDepends "${depends[@]}"
 }
@@ -29,7 +29,7 @@ function sources_basilisk() {
 
 function build_basilisk() {
     cd BasiliskII/src/Unix
-    local params=(--enable-sdl-video --enable-sdl-audio --disable-vosf --without-mon --without-esd)
+    local params=(--enable-sdl-video --enable-sdl-audio --disable-vosf --without-mon --without-esd --with-bincue --enable-standalone-gui)
     ! isPlatform "x86" && params+=(--disable-jit-compiler)
     ! isPlatform "x11" && params+=(--without-x --without-gtk)
     isPlatform "aarch64" && params+=(--build=arm)
@@ -53,6 +53,62 @@ function configure_basilisk() {
 
     mkUserDir "$md_conf_root/macintosh"
 
-    addEmulator 1 "$md_id" "macintosh" "$md_inst/bin/BasiliskII --rom $romdir/macintosh/mac.rom --disk $romdir/macintosh/disk.img --extfs $romdir/macintosh --config $md_conf_root/macintosh/basiliskii.cfg ${params[*]}"
+    # set prefs file to RetroPie path
+    if [ -e "/home/$user/.basilisk_ii_prefs" ]; then
+	rm "/home/$user/.basilisk_ii_prefs"
+    fi
+    ln -s "$md_conf_root/macintosh/basilisk.cfg" "/home/$user/.basilisk_ii_prefs"
+
+    if [ ! -f "$md_conf_root/macintosh/basilisk.cfg" ]; then
+	touch "$md_conf_root/macintosh/basilisk.cfg"
+    fi
+
+    # update BIOS ROM path
+    if [ $(grep "^rom " "$md_conf_root/macintosh/basilisk.cfg" -c) -gt 0 ]; then
+        sed -i "s|^rom.*|rom $biosdir/classicmac.rom|" "$md_conf_root/macintosh/basilisk.cfg"
+    else
+        echo "rom $biosdir/classicmac.rom" > "$md_conf_root/macintosh/basilisk.cfg"
+    fi
+
+    # default RAM to 64MB if not already set
+    if [ $(grep "^ramsize " "$md_conf_root/macintosh/basilisk.cfg" -c) -eq 0 ]; then
+        echo "ramsize 16777216" >> "$md_conf_root/macintosh/basilisk.cfg"
+    fi
+
+    # default disk to classic_disk.img if not set
+    if [ $(grep "^disk " "$md_conf_root/macintosh/basilisk.cfg" -c) -eq 0 ]; then
+        echo "disk $romdir/macintosh/classoc_disk.img" >> "$md_conf_root/macintosh/basilisk.cfg"
+    fi
+
+    # default modelid to 14 if not already set
+    if [ $(grep "^modelid " "$md_conf_root/macintosh/basilisk.cfg" -c) -eq 0 ]; then
+        echo "modelid 14" >> "$md_conf_root/macintosh/basilisk.cfg"
+    fi
+
+    # default cpu to 4 if not already set
+    if [ $(grep "^cpu " "$md_conf_root/macintosh/basilisk.cfg" -c) -eq 0 ]; then
+        echo "cpu 4" >> "$md_conf_root/macintosh/basilisk.cfg"
+    fi
+
+    # default fpu to true if not set
+    if [ $(grep "^fpu " "$md_conf_root/macintosh/basilisk.cfg" -c) -eq 0 ]; then
+        echo "fpu true" >> "$md_conf_root/macintosh/basilisk.cfg"
+    fi
+
+    # default screen to win/640/480 if not set
+    if [ $(grep "^screen " "$md_conf_root/macintosh/basilisk.cfg" -c) -eq 0 ]; then
+        echo "screen win/640/480" >> "$md_conf_root/macintosh/basilisk.cfg"
+    fi
+
+    # default frameskip to 1 if not set
+    if [ $(grep "^frameskip " "$md_conf_root/macintosh/basilisk.cfg" -c) -eq 0 ]; then
+        echo "frameskip 1" >> "$md_conf_root/macintosh/basilisk.cfg"
+    fi
+
+    # Set user to own config
+    chown "$user:$user" "$md_conf_root/macintosh/basilisk.cfg"
+
+
+    addEmulator 1 "$md_id" "macintosh" "$md_inst/bin/BasiliskII --rom $biosdir/classicmac.rom --disk $romdir/macintosh/classic_disk.img --extfs $romdir/macintosh --config $md_conf_root/macintosh/basiliskii.cfg ${params[*]}"
     addSystem "macintosh"
 }
